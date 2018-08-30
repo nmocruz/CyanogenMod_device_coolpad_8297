bd599685aee67b3089c3ca4b4c8809ed3d236076
Nuno Cruz <ncruz@softworks.com>
2018-08-30 22:05:50 +0100

    add st

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,6 +35,7 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 # Kernel
 BOARD_KERNEL_CMDLINE         := androidboot.hardware=qcom user_debug=31 zcache
 BOARD_KERNEL_BASE            := 0x80200000
+BOARD_KERNEL_IMAGE_NAME 	 := zImage
 BOARD_MKBOOTIMG_ARGS         := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE        := 2048
 TARGET_KERNEL_SOURCE         := kernel/coolpad/8860U
